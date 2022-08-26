df5c77592d7660a728ac678fc9e8c385f06ab8e5
Daniel Ohrin <xqiam@outlook.com>
2022-08-26 10:30:44 -0500

    Added Kiln.js

--- src/scripts/PotteryWheel.js
+++ src/scripts/PotteryWheel.js
@@ -1,15 +1,14 @@
 //Define a variable to have the primary key for each piece of pottery. Initial value = 1;
-const initialValue = 1;
+let idValue = 0;
 
+const idMaker = function* () {
+    idValue++;
+    yield idValue;
+}
 //Define and export a function makePottery:
     //Accept 3 parameters (shape, weight, height)
     // Return an object with shape, weight, height, and id 
     //* ID should use a generator function that adds 1 each time its called.
-    let idValue = 0;
-
-    const idMaker = function* () {
-        yield idValue++;
-    }
     
     export const makePottery = (shapeString, weightNum, heightNum) => {
         
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,26 @@
 // Imports go first
 import { makePottery } from "./PotteryWheel.js"
-
+import { firePottery } from "./Kiln.js"
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
+let mug = makePottery("mug", 12, 5);
+let pot = makePottery("pot", 40, 20);
+let cup = makePottery("cup", 8, 8);
+let vase = makePottery("vase", 25, 20);
+let plate = makePottery("plate", 10, 2);
 
 // Fire each piece of pottery in the kiln
-
+firePottery(mug, 230);
+firePottery(pot, 2200);
+firePottery(cup, 3000);
+firePottery(vase, 5789);
+firePottery(plate, 1000);
 
 // Determine which ones should be sold, and their price
 
 
 // Invoke the component function that renders the HTML list
+
 
 
 
